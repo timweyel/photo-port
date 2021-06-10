e4eda5e30f13e5464d7be4145ba604b2c778cbfa
TimWeyel <tweyel@gmail.com>
2021-06-10 11:47:53 -0700

    added PhotoLIst component and tests for Gallery and PhotoList

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -9,8 +10,10 @@ describe('About component', () => {
   it('renders', () => {
     render(<About />);
   });
+  
   it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
-  })
+  });
 })
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,7 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 function About() {
 
   return (
-    
     <section className="my-5" >
-      <a data-testid="about" href="#about"></a>
       <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
